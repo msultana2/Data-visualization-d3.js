d192bcc485b2bc01806627d53bac1e72231268a7
Arjuman Sultana <arjuman.sultana86@gmail.com>
2018-01-29 16:40:51 -0800

    delete previous files

--- js/Toolbarjs.js
+++
@@ -1,9 +0,0 @@
-// javascript that handles toolbar on click calls
-
-var visualizations = ["barchart", "scatterplot", "linechart", "bubblechart", "chordchart"];
-function showChart(chart) {
-  for(id in visualizations) {
-    document.getElementById(visualizations[id]).style.display = "none";
-  }
-  document.getElementById(chart).style.display = "block";
-}
